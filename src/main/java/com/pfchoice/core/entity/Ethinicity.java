d2798a96172685675b1a9f0de369dcf252e5a5b3
gandlurisk <skumar@pfchoice.com>
2016-01-08 00:41:43 -0500

    added provided ,insurance and ethinicity entities

---
+++ src/main/java/com/pfchoice/core/entity/Ethinicity.java
@@ -0,0 +1,183 @@
+package com.pfchoice.core.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ *
+ * @author Mohanasundharam
+ */
+@Entity(name = "lu_ethinicity")
+public class Ethinicity implements Serializable
+{
+	
+    private static final long serialVersionUID = 1L;
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name="code", nullable = false)
+    private Byte id;
+
+    @Column(name="description")
+    private String description;
+       
+    @Column(name="created_date")
+    private Timestamp createdDate;
+    
+    @Column(name="updated_date")
+    private Timestamp updatedDate;
+    
+    @Column(name="created_by")
+    private String createdBy;
+    
+    @Column(name="updated_by")
+    private String updatedBy;
+    
+    @Column(name="active_ind")
+    private char activeInd;
+    
+      
+    public Ethinicity()
+    {
+    }
+
+    public Ethinicity(Byte id)
+    {
+        this.id = id;
+    }
+
+    public Byte getId()
+    {
+        return id;
+    }
+
+    public void setId(Byte id)
+    {
+        this.id = id;
+    }
+    
+       
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the createdDate
+	 */
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	/**
+	 * @return the updatedDate
+	 */
+	public Timestamp getUpdatedDate() {
+		return updatedDate;
+	}
+
+	/**
+	 * @param updatedDate the updatedDate to set
+	 */
+	public void setUpdatedDate(Timestamp updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param credtedBy the credtedBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the updatedBy
+	 */
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	/**
+	 * @param updatedBy the updatedBy to set
+	 */
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	/**
+	 * @return the activeInd
+	 */
+	public char getActiveInd() {
+		return activeInd;
+	}
+
+	/**
+	 * @param activeInd the activeInd to set
+	 */
+	public void setActiveInd(char activeInd) {
+		this.activeInd = activeInd;
+	}
+
+	
+	
+
+	@Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ethinicity))
+        {
+            return false;
+        }
+        Ethinicity other = (Ethinicity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "com.pfchoice.core.entity.Ethinicity[ id=" + id + " ]";
+    }
+
+}
---
+++ src/main/java/com/pfchoice/core/entity/Insurance.java
@@ -0,0 +1,182 @@
+package com.pfchoice.core.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ *
+ * @author Mohanasundharam
+ */
+@Entity(name = "insurance")
+public class Insurance implements Serializable
+{
+	
+    private static final long serialVersionUID = 1L;
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name="Insurance_Id", nullable = false)
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+       
+    @Column(name="created_date")
+    private Timestamp createdDate;
+    
+    @Column(name="updated_date")
+    private Timestamp updatedDate;
+    
+    @Column(name="created_by")
+    private String createdBy;
+    
+    @Column(name="updated_by")
+    private String updatedBy;
+    
+    @Column(name="active_ind")
+    private char activeInd;
+    
+      
+    public Insurance()
+    {
+    }
+
+    public Insurance(Integer id)
+    {
+        this.id = id;
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+    
+    /**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the createdDate
+	 */
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	/**
+	 * @return the updatedDate
+	 */
+	public Timestamp getUpdatedDate() {
+		return updatedDate;
+	}
+
+	/**
+	 * @param updatedDate the updatedDate to set
+	 */
+	public void setUpdatedDate(Timestamp updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param credtedBy the credtedBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the updatedBy
+	 */
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	/**
+	 * @param updatedBy the updatedBy to set
+	 */
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	/**
+	 * @return the activeInd
+	 */
+	public char getActiveInd() {
+		return activeInd;
+	}
+
+	/**
+	 * @param activeInd the activeInd to set
+	 */
+	public void setActiveInd(char activeInd) {
+		this.activeInd = activeInd;
+	}
+
+	
+	
+
+	@Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Insurance))
+        {
+            return false;
+        }
+        Insurance other = (Insurance) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "com.pfchoice.core.entity.Insurance[ id=" + id + " ]";
+    }
+
+}
