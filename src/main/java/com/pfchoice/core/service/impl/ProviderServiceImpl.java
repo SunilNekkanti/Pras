29722df8a42ef7faa6dcb0c1b43ef37262a687ba
gandlurisk <skumar@pfchoice.com>
2016-01-12 14:54:18 -0500

    added findAll method

--- src/main/java/com/pfchoice/core/service/impl/ProviderServiceImpl.java
+++ src/main/java/com/pfchoice/core/service/impl/ProviderServiceImpl.java
@@ -2,6 +2,9 @@ package com.pfchoice.core.service.impl;
 
 import ml.rugal.sshcommon.hibernate.Updater;
 import ml.rugal.sshcommon.page.Pagination;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,6 +61,12 @@ public class ProviderServiceImpl implements ProviderService
     {
         Updater<Provider> updater = new Updater<>(bean);
         return providerDao.updateByUpdater(updater);
+    }
+    
+    @Override
+    public List<Provider> findAll()
+    {
+    	return providerDao.findAll();
     }
 
 }
