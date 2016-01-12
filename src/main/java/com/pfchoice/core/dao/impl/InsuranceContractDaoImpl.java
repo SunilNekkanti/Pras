49016c8acb48965bd5cc4b7a3ca36a4c2466230d
gandlurisk <skumar@pfchoice.com>
2016-01-12 14:44:17 -0500

    Added insurance and provider DAO Implementations

---
+++ src/main/java/com/pfchoice/core/dao/impl/InsuranceContractDaoImpl.java
@@ -0,0 +1,76 @@
+package com.pfchoice.core.dao.impl;
+
+import ml.rugal.sshcommon.hibernate.HibernateBaseDao;
+import ml.rugal.sshcommon.page.Pagination;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.FetchMode;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import com.pfchoice.core.dao.InsuranceContractDao;
+import com.pfchoice.core.entity.InsuranceContract;
+
+/**
+ *
+ * @author Mohanasundharam
+ */
+@Repository
+public class InsuranceContractDaoImpl extends HibernateBaseDao<InsuranceContract, Integer> implements InsuranceContractDao
+{
+
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(InsuranceContractDaoImpl.class
+        .getName());
+
+    @Override
+    public Pagination getPage(int pageNo, int pageSize)
+    {
+        Criteria crit = createCriteria();
+        Pagination page = findByCriteria(crit, pageNo, pageSize);
+        return page;
+    }
+
+    @Override
+    public InsuranceContract findById(Integer id)
+    {
+    	InsuranceContract entity = get(id);
+        return entity;
+    }
+
+    @Override
+    public InsuranceContract save(InsuranceContract bean)
+    {
+        getSession().save(bean);
+        return bean;
+    }
+
+    @Override
+    public InsuranceContract deleteById(Integer id)
+    {
+//        throw new UnsupportedOperationException();
+    	InsuranceContract entity = super.get(id);
+        if (entity != null)
+        {
+            getSession().delete(entity);
+        }
+        return entity;
+    }
+
+    @Override
+    protected Class<InsuranceContract> getEntityClass()
+    {
+        return InsuranceContract.class;
+    }
+
+    @SuppressWarnings("unchecked")
+	public List<InsuranceContract> findAll()
+    {
+    	
+    	Criteria cr = getSession().createCriteria(getEntityClass());
+    	cr.setFetchMode("Insurance", FetchMode.JOIN);
+    	List<InsuranceContract> list = cr.list();
+    
+    	return list;
+    }
+}
