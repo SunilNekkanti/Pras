3ee94c3400e3500141a0997a49acc7c357c50f24
gandlurisk <skumar@pfchoice.com>
2016-01-21 23:58:32 -0500

    add entity for ZipCode

---
+++ src/main/java/com/pfchoice/core/entity/ZipCode.java
@@ -0,0 +1,190 @@
+package com.pfchoice.core.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author sarath
+ */
+@Entity
+@Table(name = "lu_state_zip")
+public class ZipCode implements Serializable
+{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name="zipcode", nullable = false)
+    private Integer code;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="created_date")
+    private Date createdDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="updated_date")
+    private Date updatedDate;
+    
+    @Column(name="created_by")
+    private String createdBy;
+    
+    @Column(name="updated_by")
+    private String updatedBy;
+    
+    @Column(name="active_ind")
+    private char activeInd;
+    
+    @OneToOne(  fetch = FetchType.LAZY, orphanRemoval = true)
+    @JoinColumn(name="statecode", referencedColumnName="code")
+    private State stateCode;
+    
+    
+    public ZipCode()
+    {
+    }
+
+    public ZipCode(Integer code)
+    {
+        this.code = code;
+    }
+
+    public Integer getCode()
+    {
+        return code;
+    }
+
+    public void setCode(Integer code)
+    {
+        this.code = code;
+    }
+    
+	/**
+	 * @return the stateCode
+	 */
+	public State getStateCode() {
+		return stateCode;
+	}
+
+	/**
+	 * @param stateCode the stateCode to set
+	 */
+	public void setStateCode(State stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	/**
+	 * @return the updatedDate
+	 */
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	/**
+	 * @param updatedDate the updatedDate to set
+	 */
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param credtedBy the credtedBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	/**
+	 * @return the updatedBy
+	 */
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	/**
+	 * @param updatedBy the updatedBy to set
+	 */
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	/**
+	 * @return the activeInd
+	 */
+	public char getActiveInd() {
+		return activeInd;
+	}
+
+	/**
+	 * @param activeInd the activeInd to set
+	 */
+	public void setActiveInd(char activeInd) {
+		this.activeInd = activeInd;
+	}
+
+	@Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (code != null ? code.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the code fields are not set
+        if (!(object instanceof ZipCode))
+        {
+            return false;
+        }
+        ZipCode other = (ZipCode) object;
+        if ((this.code == null && other.code != null) || (this.code != null && !this.code.equals(other.code)))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "com.pfchoice.core.entity.State[ code=" + code + " ]";
+    }
+
+}
