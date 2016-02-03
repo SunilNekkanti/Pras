cdef3f629b05fa7416bc0efed3572f9c5447c5ad
gandlurisk <skumar@pfchoice.com>
2016-02-03 10:49:56 -0500

    added Ethinicity Formatter

---
+++ src/main/java/com/pfchoice/core/entity/formatter/EthinicityFormatter.java
@@ -0,0 +1,32 @@
+package com.pfchoice.core.entity.formatter;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import com.pfchoice.core.entity.Ethinicity;
+import com.pfchoice.core.service.EthinicityService;
+
+@Component
+public class EthinicityFormatter implements  Formatter<Ethinicity> {
+
+     @Autowired
+     private EthinicityService ethinicityService;
+     //Some service class which can give the CountyService after
+     //fetching from Database
+ 
+     @Override
+     public String print(Ethinicity ethinicity, Locale arg1) {
+           return ethinicity.getDescription();
+     }
+ 
+     @Override
+      public Ethinicity parse(String id, Locale arg1) throws ParseException {
+           return ethinicityService.findById(Byte.parseByte(id));
+           //Else you can just return a new object by setting some values
+           //which you deem fit.
+      }
+}
